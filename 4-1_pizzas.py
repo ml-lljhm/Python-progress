85ee52771008f697d91aca0cf1fd004de588b8b2
ml-lljhm <emil.liljehem@outlook.com>
2025-12-01 12:40:57 +0100

    Add files via upload

---
+++ 4-1_pizzas.py
@@ -0,0 +1,4 @@
+favorite_pizza = ['margherita', 'calzone', 'hawaii']
+for pizza in favorite_pizza:
+    print(f"{pizza.title()} is one of my favorite pizzas")
+print("\nI really love to eat pizza.")
