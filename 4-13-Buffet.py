47405d92d5252d9bf5e3db9cbb77f93c66a95bc5
ml-lljhm <emil.liljehem@outlook.com>
2025-12-04 22:05:19 +0100

    Add files via upload

---
+++ 4-13-Buffet.py
@@ -0,0 +1,8 @@
+foods = ('pizza', 'sushi', 'kebab', 'hamburger', 'stew')
+print("This restaurant serves the following food:")
+for food in foods:
+    print(food.title())
+foods = ('sausage', 'cat food', 'kebab', 'hamburger', 'stew')
+print("The new revised menu of the restaurant is the following:")
+for food in foods:
+    print(food.title())
